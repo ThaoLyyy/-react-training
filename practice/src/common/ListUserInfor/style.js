54083e7bdc2b979314008fc3f216f1918e919096
ThaoLy1805 <89328896+ThaoLyyy@users.noreply.github.com>
2022-08-19 17:54:17 +0700

    Update  ListUserInfor

--- practice/src/common/ListUserInfor/style.js
+++ practice/src/common/ListUserInfor/style.js
@@ -1,49 +1,50 @@
-
 import styled from "styled-components";
 
 const ListItem = styled.div`
-  width: 900px;
+  width: 1020px;
   height: auto;
   display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  margin-left: 300px;
-  margin: 70px 0px 0px 364px;
+  flex-flow: row wrap;
+  justify-content: space-evenly;
+  margin-left: 330px;
+  margin-top: 40px;
 `;
 const Item = styled.div`
   display: flex;
   flex-direction: row;
-  width: 325px;
+  width: 450px;
   margin-bottom: 30px;
-  margin-left: 79px;
+  /* margin-left: 79px; */
   background: #ffdede;
   border: 1px solid #fff;
   padding: 15px;
+  border-radius: 10px;
+  flex: 0.2 1;
 `;
 const ImageWrapper = styled.div`
   display: flex;
   flex-direction: column;
 `;
 const ImageItem = styled.img`
-  width: 170px;
-  height: 215px;
-
-   object-fit: fill;
+  width: 165px;
+  height: 165px;
+  object-fit: fill;
+  padding: 10px;
 `;
-const InforItem  = styled.div`
+const InforItem = styled.div`
   display: flex;
   flex-direction: column;
-  margin-left: 20px;
-  line-height: 50px;
+  /* margin-left: 20px; */
+  line-height: 45px;
 `;
-const NameItem = styled.p`
-  font-size: 28px;
-  font-weight: 700;
-  color: #0f1a42;
-`;
+// const NameItem = styled.p`
+//   font-size: 28px;
+//   font-weight: 700;
+//   color: #0f1a42;
+// `;
 const DescrItem = styled.p`
-  font-size: 19px;
-  font-weight: 600;
+  font-size: 16px;
+  font-weight: 500;
 `;
 
 const BtnIcon = styled.div`
@@ -67,16 +68,23 @@ const BtnDel = styled.button`
   border: none;
   cursor: pointer;
 `;
+const FontBtnIcon = styled.button`
+  font-size: 25px;
+  border: none;
+  background-color: #78e2f3;
+  margin-top: 10px;
+  cursor: pointer;
+`;
 
 export {
   ListItem,
   Item,
   ImageWrapper,
   ImageItem,
-  InforItem ,
-  NameItem,
+  InforItem,
   DescrItem,
   BtnIcon,
+  FontBtnIcon,
   BtnEdit,
   BtnDel,
 };
