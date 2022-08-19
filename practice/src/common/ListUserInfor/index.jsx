f1ebf3681f8212db4854bdaf828c98a50da54961
ThaoLy1805 <89328896+ThaoLyyy@users.noreply.github.com>
2022-08-19 17:57:31 +0700

    update listUserInfor

--- practice/src/common/ListUserInfor/index.jsx
+++ practice/src/common/ListUserInfor/index.jsx
@@ -1,172 +1,59 @@
 import { useState, useContext } from "react";
 import { FaEdit } from "react-icons/fa";
 import { RiDeleteBinLine } from "react-icons/ri";
-// import { listUsers } from "../../../data";
-import { StoreContext } from "../../store";
 import Button from "../Button";
-// import {
-//   ListItem,
-//   ConstactList,
-//   CarBody,
-//   ImageWrapper,
-//   Image,
-//   ItemInfo,
-//   NameItem,
-//   InforItem,
-//   BtnEdit,
-//   BtnDel,
-// } from "./style";
+
 
 import {
   ListItem,
   Item,
   ImageWrapper,
   ImageItem,
-  InforItem ,
+  InforItem,
   DescrItem,
   BtnIcon,
-  BtnEdit,
-  BtnDel,
+  
 } from "./style";
 
-const ListUserInfor = () => {
-  // const [users, setUsers] = useState([
-  //   {
-  //     id: 1,
-  //     username: "Nguyen Van A",
-  //     email: "anguyen123@mail.com",
-  //     phone: "(+84)5555-2222",
-  //     address: "89 HC, DN",
-  //   },
-  //   {
-  //     id: 2,
-  //     username: "Le Thi B",
-  //     email: "ble456@mail.com",
-  //     phone: "(+84)4555-5735",
-  //     address: "57 DD, HN",
-  //   },
-  //   {
-  //     id: 3,
-  //     username: "Phan Thi C",
-  //     email: "cphan789@mail.com",
-  //     phone: "(+84)6555-9931",
-  //     address: "25 BT,HCM",
-  //   },
-  //   {
-  //     id: 4,
-  //     username: "Dang Van D",
-  //     email: "ddang333@mail.com",
-  //     phone: "(+84)6619-5731",
-  //     address: "67 TT, QN",
-  //   },
-  //   {
-  //     id: 5,
-  //     username: "Tran Van E",
-  //     email: "etran555@mail.com",
-  //     phone: "(+84)6631-2097",
-  //     address: "73 LD, DL",
-  //   },
-  // ]);
+const ListUserInfor = ({users}) => {
+  const [selectedDelUserId, setSelectedDelUserId] = useState(null);
 
-  // const [state, dispatch] = useStore(StoreContext)
+ // Show DeletePopup
+ const handleOpen = (id) => {
+  setSelectedDelUserId(id);
+};
+// Close popup
+const handleClose = () => {
+  setSelectedDelUserId(null);
+};
 
-  // const [state, dispatch] = useStore(StoreContext)
-
-  // let InforUsers = [].concat(listUsers, state.users)
-  // console.log(InforUsers)
-
-  // localStorage.setItem('listProduct', JSON.stringify(InforUsers))
-  // let listUser = JSON.parse(localStorage.getItem('listUser'))
-  // console.log(listUser)
-
-  // -----------
-  const { users } = useContext(StoreContext);
-  const [show, setShow] = useState(false);
-  console.log(users);
-  // Show DeletePopup
-  const handleOpen = () => {
-    setShow(!show);
-  };
-  // Close DeletePopup
-  const handleClose = () => {
-    setShow(false);
-  };
-
-  // return (
-  // <h1>aaaa</h1>
-
-  // <ListItem>
-  //   {users.map((user, index) => (
-  //     <ConstactList key={index}>
-  //       {/* <CarBody>
-  //         <ImageWrapper>
-  //           <Image src={user.userImg} />
-  //         </ImageWrapper>
-  //         <ItemInfo>
-  //           <NameItem>
-  //             Username:
-  //             <InforItem> Nguyen Van A</InforItem>
-  //           </NameItem>
-  //           <NameItem>
-  //             Phone:
-  //             <InforItem> 0123456789</InforItem>
-  //           </NameItem>
-  //           <NameItem>
-  //             Email:
-  //             <InforItem> anguyen@123gmail.com</InforItem>
-  //           </NameItem>
-  //           <NameItem>
-  //             Address:
-  //             <InforItem> Da Nang</InforItem>
-  //           </NameItem>
-  //           <BtnEdit>
-  //             <FaEdit />
-  //           </BtnEdit>
-  //           <BtnDel onClicked={handleOpen}>
-  //             <RiDeleteBinLine />
-  //           </BtnDel>
-
-  //         </ItemInfo>
-  //       </CarBody> */}
-  //     </ConstactList>
-  //   ))}
-  // </ListItem>
-  // );
-
-  // ====
   return (
     <ListItem>
-      {users.map((user, index) => (
-        <Item key={index}>
+      {users.map((user) => (
+        <Item key={user.id}>
           <ImageWrapper>
-            <ImageItem src={user.userImage} />         
+            <ImageItem src={user.image} />
           </ImageWrapper>
-          <InforItem >
+          <InforItem>
             <DescrItem>Username: {user.userName} </DescrItem>
-            <DescrItem>Phone: {user.userPhone}</DescrItem>
-            <DescrItem>Email: {user.userEmail}</DescrItem>
-            <DescrItem>Address: {user.userAddress}</DescrItem>
+            <DescrItem>Phone: {user.userPhone} </DescrItem>
+            <DescrItem>Email: {user.userEmail} </DescrItem>
+            <DescrItem>Address: {user.userAddress} </DescrItem>
             <BtnIcon>
-              <BtnEdit className="edit">
+              <Button className="edit">
                 <FaEdit />
-              </BtnEdit>
-              <BtnDel className='delete'>
+              </Button>
+              <Button className="delete" onClicked={handleOpen}>
                 <RiDeleteBinLine />
-              </BtnDel>
-              {/* <Button className='edit' icon='fas fa-edit'>  */}
-              {/* </Button> */}
-              {/* <Button
-                            onClicked={handleOpen}
-                            className='delete'
-                            icon='fas fa-trash-alt'
-                        > */}
-
-              {/* </Button> */}
+              </Button>           
               {/* {show && <DeletePopup onClosePopup={handleClose} />} */}
             </BtnIcon>
           </InforItem>
         </Item>
       ))}
+      {/* {!!selectedDelUserId && (
+        <ConfirmDeletePopup onOK={handleDelete} onClosePopup={handleClose} />
+      )} */}
     </ListItem>
   );
 };
